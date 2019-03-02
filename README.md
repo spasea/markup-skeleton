22adbe05e5405f0e3cc1940ecdd7f9355d170b83
ithespv <ithespv@gmail.com>
2019-03-02 12:55:18 +0200

    Init all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is markup-skeleton 
+
+## To start developing
+- npm install
+- serve -s .
---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('it\'s from index.js');
+});
